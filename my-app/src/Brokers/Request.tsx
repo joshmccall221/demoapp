cdf82ae76346051406b8d5f37cacf94c91bf8711
Josh McCall <jomccal@microsoft.com>
2022-06-14 18:16:12 -0500

    wip

--- my-app/src/Brokers/Request.tsx
+++ my-app/src/Brokers/Request.tsx
@@ -25,16 +25,34 @@ var request = {
     },
 }
 
-export const getRequestFromAPI = (): Promise<Request[]> => {
-    return mocker()
-        .schema('request', request, 200)
-        .build()
-        .then(({ request }: any) => {
-            console.log({ request })
-            return request
-        },
-            (reason: any) => {
-                console.log({ reason })
+export const getRequestFromAPI = async (): Promise<Request[]> => {
 
-            });
+    window.fetch('microsoft.com/api/requests',
+        {
+            // learn more about this API here: https://graphql-pokemon2.vercel.app/
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+            },
+        })
+        .then(function(response) {
+            return response.json();
+          }).then(function(data) {
+            console.log(data); 
+            return data // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+          });
+    //const {data, errors} = await response.json()
+    return [];
+
+    // return mocker()
+    //     .schema('request', request, 200)
+    //     .build()
+    //     .then(({ request }: any) => {
+    //         console.log({ request })
+    //         return request
+    //     },
+    //         (reason: any) => {
+    //             console.log({ reason })
+
+    //         });
 };
